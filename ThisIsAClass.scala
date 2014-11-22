1778e1961e05b88abda21b5cdadf57b2aad66afe
Richard Wong <rk2wong@uwaterloo.ca>
2014-11-21 19:54:01 -0500

    simulating object fields

--- ThisIsAClass.scala
+++ ThisIsAClass.scala
@@ -1,6 +1,7 @@
 class Complex(real: Double, imaginary: Double) {
-	def re() = real // this is a getter method which simply returns the real component of the complex number
+	def re = real // this is a getter method which simply returns the real component of the complex number
 	// the compiler infers that the return type of this function is Double, but it will not always be able to do this
-	def im() = imaginary
+	def im = imaginary
 	// later, if you want to use these methods, they have to have the open and close parentheses
+	// update 1: making these into methods without arguments (different from zero arguments) allows them to be accessed like fields of an object
 }
--- UsingThatClass.scala
+++ UsingThatClass.scala
@@ -2,6 +2,7 @@ object ComplexNumbers {
 	def main(args: Array[String]) {
 		val c = new Complex(1.2, 3.4) // when we execute ComplexNumbers, the thing automatically checks the classpath for a Complex.class
 		// so as long as you compile the Complex class at some point, you're good
-		println("the number is " + c.re() + " " + c.im() + "i")
+		println("the number is " + c.re + " " + c.im + "i")
+		// update 1: now that in the class Complex these are methods without arguments, we can access them like fields
 	}
 }
